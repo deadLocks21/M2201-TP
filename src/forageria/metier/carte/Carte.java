628a89b13ab859d08816fc21c201c31208a7fb79
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-05-13 09:37:28 +0200

    [TP02] Question 5, implementation de la classe Carte

--- src/forageria/metier/carte/Carte.java
+++ src/forageria/metier/carte/Carte.java
@@ -1,7 +1,42 @@
 package forageria.metier.carte;
 
+import forageria.metier.carte.cases.Case;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Classe en charge de stocker la carte.
  */
 public class Carte {
+    /**
+     * Stocke la taille de la carte.
+     */
+    private int taille;
+    /**
+     * Variable permettant de stocker les cases de notre jeu.
+     */
+    public Map<Coordonnee,Case> cases;
+
+
+    /**
+     * Constructeur de la classe Carte.
+     *
+     *
+     * @param messageRecu Message recu par l'IA.
+     */
+    public Carte(String messageRecu){
+        cases = new HashMap<>();
+    }
+
+
+    /**
+     * Assesseur de la variable taille.
+     *
+     *
+     * @return Taille de la carte.
+     */
+    public int getTaille() {
+        return taille;
+    }
 }
