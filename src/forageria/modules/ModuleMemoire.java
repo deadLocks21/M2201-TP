bc2c4860d8e706004ae8ace59df847538c056cac
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-05-13 10:59:35 +0200

    [TP02] Question 21, modification de la classe Memoire.

--- src/forageria/modules/ModuleMemoire.java
+++ src/forageria/modules/ModuleMemoire.java
@@ -1,15 +1,48 @@
 package forageria.modules;
 
 import forageria.IA;
+import forageria.metier.carte.Carte;
 
 /**
  * Module en charge de la mémorisation et de la restitution des informations obtenues
  * @author Matthieu
  */
 public class ModuleMemoire extends Module  {
+    /**
+     * Stocke la carte du jeu.
+     */
+    Carte carte;
 
+
+    /**
+     * Construction du ModuleMemoire
+     *
+     *
+     * @param ia IA qui a initialisé ce module.
+     */
     public ModuleMemoire(IA ia) {
         super(ia);
+    }
+
+
+    /**
+     * Permet de génerer la carte avec le message recu.
+     *
+     *
+     * @param messageRecu Message du serveur contenant la carte.
+     */
+    public void genererCarte(String messageRecu){
+        carte = new Carte(messageRecu);
+    }
+
+    /**
+     * Vaut TRUE si la carte est settée.
+     *
+     *
+     * @return TRUE | FALSE
+     */
+    public boolean hasCarte(){
+        return carte != null;
     }
 
 }
