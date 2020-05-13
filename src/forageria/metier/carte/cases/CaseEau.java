fcba3f2f3dffbebf59ffad131556965fb35f033d
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-05-13 09:56:01 +0200

    [TP02] Question 8, implémentation de CaseHerbe et CaseEau.

---
+++ src/forageria/metier/carte/cases/CaseEau.java
@@ -0,0 +1,14 @@
+package forageria.metier.carte.cases;
+
+import forageria.metier.carte.Coordonnee;
+
+public class CaseEau extends Case {
+    /**
+     * Permet d'initialiser une case d'eau.
+     *
+     * @param coordonnee Coordonnées de la case.
+     */
+    public CaseEau(Coordonnee coordonnee) {
+        super(coordonnee);
+    }
+}
---
+++ src/forageria/metier/carte/cases/CaseHerbe.java
@@ -0,0 +1,14 @@
+package forageria.metier.carte.cases;
+
+import forageria.metier.carte.Coordonnee;
+
+public class CaseHerbe extends Case {
+    /**
+     * Permet d'initialiser une case d'herbe.
+     *
+     * @param coordonnee Coordonnées de la case.
+     */
+    public CaseHerbe(Coordonnee coordonnee) {
+        super(coordonnee);
+    }
+}
