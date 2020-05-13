7172364df10c5ec5050a1fae6af5a7c8dfdd66fc
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-05-13 10:15:07 +0200

    [TP02] Question 13, implémentation de l'en-tête demandée.

--- src/forageria/metier/carte/ressources/FabriqueRessource.java
+++ src/forageria/metier/carte/ressources/FabriqueRessource.java
@@ -1,4 +1,21 @@
 package forageria.metier.carte.ressources;
 
+import forageria.metier.carte.cases.Case;
+
+/**
+ * Classe permettant de fabriquer des ressources.
+ */
 public class FabriqueRessource {
+    /**
+     * Méthode permettant de retourner une ressource en fonction de lettre.
+     *
+     *
+     * @param position  Position de la case à créer.
+     * @param lettre    Identifiant de la ressource à créer.
+     *
+     * @return Ressource créée en fonction de la lettre.
+     */
+    public static Ressource creer(Case position, Character lettre){
+        return Ressource;
+    }
 }
