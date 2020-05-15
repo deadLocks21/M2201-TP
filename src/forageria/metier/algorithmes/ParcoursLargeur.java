812f382793dc44fff96e36fcda8d8241e5100bce
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-05-15 08:57:16 +0200

    [TP04] Question 6, création de la classe ParcoursLargeur().

---
+++ src/forageria/metier/algorithmes/ParcoursLargeur.java
@@ -0,0 +1,20 @@
+package forageria.metier.algorithmes;
+
+import forageria.metier.carte.Carte;
+import forageria.metier.carte.cases.Case;
+
+public class ParcoursLargeur extends AlgorithmeCalculDistance {
+    /**
+     * Constructur de la classe.
+     *
+     * @param carte Représente la carte stocké par la classe.
+     */
+    public ParcoursLargeur(Carte carte) {
+        super(carte);
+    }
+
+    @Override
+    public void calculerDistancesDepuis(Case depart) {
+
+    }
+}
