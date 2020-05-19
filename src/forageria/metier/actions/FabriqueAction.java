cf6fd6e12a36ae38279bec726629a4eb723a4da1
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-05-19 10:36:40 +0200

    [TP05] Question 6, implémentation de la méthode creerMouvement().

--- src/forageria/metier/actions/FabriqueAction.java
+++ src/forageria/metier/actions/FabriqueAction.java
@@ -6,4 +6,15 @@ import forageria.metier.TypeMouvement;
  * Fabrique à actions.
  */
 public class FabriqueAction {
+    /**
+     * Permet de créer le mouvement souhaité.
+     *
+     *
+     * @param mouvement Mouvement que l'on désire.
+     *
+     * @return Mouvement correspondant.
+     */
+    public static Action creerMouvement(TypeMouvement mouvement){
+        return new ActionMouvement(mouvement);
+    }
 }
