8ceb4771b81882b9121f15c9e4d2aabd803dd777
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-05-19 10:25:03 +0200

    [TP05] Question 1 et 2, ajout du package actions et de la classe Action.

---
+++ src/forageria/metier/actions/Action.java
@@ -0,0 +1,14 @@
+package forageria.metier.actions;
+
+/**
+ * Classe abstraite d'une action.
+ */
+public abstract class Action {
+    /**
+     * Permet de récupérer un message.
+     *
+     *
+     * @return Message sous forme de chaine de caractères.
+     */
+    public abstract String getMessage() ;
+}
