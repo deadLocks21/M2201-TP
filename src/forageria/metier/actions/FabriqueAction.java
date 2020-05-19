277b260177dbb21198a814575d87b1620411aeb5
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-05-19 10:41:02 +0200

    [TP05] Question 7, implémentation de la méthode AtcionDemande().

--- src/forageria/metier/actions/FabriqueAction.java
+++ src/forageria/metier/actions/FabriqueAction.java
@@ -17,4 +17,27 @@ public class FabriqueAction {
     public static Action creerMouvement(TypeMouvement mouvement){
         return new ActionMouvement(mouvement);
     }
+
+    /**
+     * Permet de créer une ActionDemande en fonction de la demande.
+     *
+     *
+     * @param demande Demande que l'on veut.
+     *
+     * @return ActionDemande avec le message correspondant.
+     */
+    public static Action creerDemande(TypeDemande demande){
+        ActionDemande actionDemande = null;
+
+        switch (demande){
+            case JOUEUR:
+                actionDemande = new ActionDemande("JOUEUR");
+                break;
+            case CARTE:
+                actionDemande = new ActionDemande("CARTE");
+                break;
+        }
+
+        return actionDemande;
+    }
 }
