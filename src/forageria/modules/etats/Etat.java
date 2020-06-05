0f7b940a875436676b40a301201d58dd37b4aeec
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-06-05 09:01:14 +0200

    [TP07] Question 2, implémentation de la classe des etats et de l'état initial.

---
+++ src/forageria/modules/etats/Etat.java
@@ -0,0 +1,64 @@
+package forageria.modules.etats;
+
+import forageria.modules.ModuleDecision;
+import forageria.modules.ModuleMemoire;
+
+/**
+ * Représente un état de notre automate.
+ */
+public abstract class Etat {
+    /**
+     * Stocke le module de décision.
+     */
+    private ModuleDecision module;
+    /**
+     * Stocke le module mémoire.
+     */
+    private ModuleMemoire memoire;
+
+
+    /**
+     * Constructeur d'un état.
+     *
+     *
+     * @param module Valeur de la variable module.
+     */
+    public Etat(ModuleDecision module){
+        this.module = module;
+    }
+
+
+    /**
+     * Assesseur de la variable module.
+     *
+     *
+     * @return Variable Module.
+     */
+    public ModuleDecision getModule() {
+        return module;
+    }
+
+    /**
+     * Assesseur de la variable Memoire.
+     *
+     *
+     * @return Variable Memoire.
+     */
+    public ModuleMemoire getMemoire() {
+        return memoire;
+    }
+
+
+    /**
+     * Code une transition vers un autre Etat.
+     *
+     *
+     * @return  Nouvel Etat.
+     */
+    public abstract Etat transition();
+
+    /**
+     * Action que doit effectuer notre Etat.
+     */
+    public abstract void action();
+}
---
+++ src/forageria/modules/etats/EtatInitial.java
@@ -0,0 +1,28 @@
+package forageria.modules.etats;
+
+import forageria.modules.ModuleDecision;
+
+/**
+ * Etat initial de notre automate.
+ */
+public class EtatInitial extends Etat {
+    /**
+     * Constructeur de l'état initial.
+     *
+     * @param module Valeur de la variable module.
+     */
+    public EtatInitial(ModuleDecision module) {
+        super(module);
+    }
+
+
+    @Override
+    public Etat transition() {
+        return null;
+    }
+
+    @Override
+    public void action() {
+
+    }
+}
